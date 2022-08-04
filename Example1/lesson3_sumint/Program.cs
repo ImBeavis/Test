d4ad88e4b06e133bc88cad0e89d2d3f941f1c4da
Andrey <Plyakin.andrei@yandex.ru>
2022-08-04 21:28:53 +0400

    lesson3_allRandom

--- Example1/lesson3_sumint/Program.cs
+++ Example1/lesson3_sumint/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberB = 7;
+﻿int numberA = new Random().Next(3, 30);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(2, 20);
+Console.WriteLine(numberB);
 int result = (numberA + numberB);
 Console.Write(result);
