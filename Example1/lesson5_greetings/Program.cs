0cbf6535f892ea00c524e9a9e305c2b191ad5487
Andrey <Plyakin.andrei@yandex.ru>
2022-08-04 21:51:47 +0400

    lesson5_greetings

---
+++ Example1/lesson5_greetings/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "андрей")
+{
+    Console.WriteLine("Ураа, это же Андрей!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
